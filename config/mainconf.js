33edc69a5f80e4cd218992403763a8f7550a71a8
Anson Zhao <ansonzhao@Ronald-Fortunatos-MacBook-Pro.local>
2018-09-24 14:18:43 -0400

    Initial commit

---
+++ config/mainconf.js
@@ -0,0 +1,57 @@
+// config/database.js
+var configGlobal = {
+    'commondb_connection': {
+        'multipleStatements': true,
+        'connectionLimit' : 100,
+        'host': '127.0.0.1',
+        'user': 'AppUser',
+        'password': 'Special888%',
+        'port'    :  3306
+    },
+    'session_connection': {
+        'multipleStatements': true,
+        'connectionLimit' : 100,
+        'host': '127.0.0.1',
+        'user': 'AppUser',
+        'password': 'Special888%',
+        'port'    :  3306
+    },
+
+    'Session_db': 'USGS',
+    'Login_db': 'USGS',
+    'Login_table': 'UserLogin',
+    'Upload_db': 'USGS',
+
+    'Server_Port': 9085,
+
+    // 'local_URL' : "",
+    // 'local_URL' : "http://viewer.usgs.aworldbridgelabs.com",
+
+    //upload file--pending
+    // 'Upload_Path': 'http://usgs.aworldbridgelabs.com/uploadfiles',
+    'Upload_Path':'uploadfiles',
+    // 'Upload_Dir': '/var/www/usgs/uploadfiles',
+    'Upload_Dir': 'uploadfiles',
+
+    //approve file--active
+    // 'GeoData_Dir': '/usr/share/worldwind-geoserver-0.2.1/data_dir/data/USGS'
+    'GeoData_Dir':'b',
+
+    //trashfolder file--trashfolder
+    'Delete_Dir':'trashfolder',
+
+    // uswtdb eye distance for placemark layer menu display (km)
+    'eyeDistance_PL': 1500,
+
+    // uswtdb eye distance for display heatmap until eyeDistance_Heatmap less than 4500 (km)
+    'eyeDistance_Heatmap': 4500,
+
+    // uswtdb initial eye distance (m)
+    'eyeDistance_initial': 5000000
+};
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = configGlobal;
+} else {
+    window.config = configGlobal;
+}
